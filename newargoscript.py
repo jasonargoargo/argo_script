d4c43f902ef891165538a2a9bef3c1f2927bdb8e
Jason Argo <36310363+jasonargoargo@users.noreply.github.com>
2018-02-22 11:46:40 -0500

    Add files via upload

--- newargoscript.py
+++ newargoscript.py
@@ -2,9 +2,7 @@ import zipfile
 import urllib.request as req
 import xml.etree.ElementTree as ET
 import glob
-import io
 import sys
-import csv
 
 
 FILENAME = ('BILLS-115-2-hconres.zip')
@@ -21,11 +19,14 @@ def retrieve_urls(filename):
 def read_xml(xml_files):
     xml_files = glob.glob('*.xml')
     for x in xml_files:
-        with open(x, 'r', encoding='utf8') as xml_parse:
-            tree = ET.parse(xml_parse)
-            legis_num = tree.findall('resolution/form')
-            for lenu in legis_num:
-                print(lenu.find('legis-num').text)
+        try:
+            with open(x, 'r', encoding='utf8') as xml_parse:
+                tree = ET.parse(xml_parse)
+                legis_num = tree.findall('resolution/form')
+                for lenu in legis_num:
+                    print(lenu.find('legis-num').text)
+        except ET.ParseError as e:
+            pass
 
 
 def main(filename):
