19226ee0381fc9f23214fdafea5a0317d987d1cb
Jason Argo <36310363+jasonargoargo@users.noreply.github.com>
2018-03-01 13:37:47 -0500

    Add files via upload

--- argoscript3.py
+++ argoscript3.py
@@ -3,10 +3,8 @@ import zipfile
 import urllib.request as req
 import xml.etree.ElementTree as ET
 import glob
-import io
 import sys
 import os
-import re
 
 
 FILENAME = ('BILLS-115-2-hconres.zip')
@@ -35,41 +33,120 @@ def open_zip(filename):
 def read_xml(xml_files):
     xml_files = glob.glob('*.xml')
     for x in xml_files:
-        # try:
         with open(x, 'r', encoding='utf8') as xml_parse:
-            legis_num = []
-            congress = []
-            session = []
-            text = []
+            hconres_dms_id = []
+            hconres_number = []
+            hconres_title = []
+            hconres_congress = []
+            hconres_session = []
+            hconres_stage = []
+            hconres_type = []
+            hconres_origin = []
+            hconres_pub_pri = []
+            hconres_text = []
+            amend_number = []
+            amend_congress = []
+            amend_session = []
+            amend_stage = []
+            amend_degree = []
+            amend_type = []
+            amend_origin = []
+            amend_text = []
             for event, elem in ET.iterparse(xml_parse):
-                if elem.tag == 'legis-num' and event == 'end':
-                    legis_num.append(elem.text)
+                # CONCURRENT RESOLUTIONS
+                if elem.tag == 'resolution' and event == 'end':
+                    hconres_dms_id.append(elem.get('dms-id'))
+                    hconres_origin.append(elem.get('key'))
+                    hconres_pub_pri.append(elem.get('public-private'))
+                    hconres_stage.append(elem.get('resolution-stage'))
+                    hconres_type.append(elem.get('resolution-type'))
+                elif elem.tag == 'legis-num' and event == 'end':
+                    hconres_number.append(elem.text)
+                elif elem.tag == 'official-title' and event == 'end':
+                    hconres_title.append(elem.text)
                 elif elem.tag == 'congress' and event == 'end':
                     raw_congress = []
                     raw_congress.append(elem.text)
                     for r in raw_congress:
-                        if elem.text == '115th CONGRESS':
-                            congress.append(
-                                re.sub(elem.text, '115th', r))
-                        elif elem.text == 'One Hundred Fifteenth Congress of the United States of America':
-                            congress.append(
-                                re.sub(elem.text, '115th', r))
+                        if elem.text in ['115th CONGRESS']:
+                            hconres_congress.append(elem.text.replace(
+                                '115th CONGRESS', '115th'))
+                        elif elem.text in ['One Hundred Fifteenth Congress of the United States of America']:
+                            hconres_congress.append(elem.text.replace(
+                                'One Hundred Fifteenth Congress of the United States of America', '115th'))
+                        else:
+                            print('You missed a congress!')
                 elif elem.tag == 'session' and event == 'end':
                     raw_session = []
                     raw_session.append(elem.text)
                     for r in raw_session:
-                        if elem.text == '2d Session':
-                            session.append(re.sub(elem.text, '2nd', r))
-                        elif elem.text == 'At the Second Session':
-                            session.append(re.sub(elem.text, '2nd', r))
-                elif elem.tag == 'text' and event == 'end':
-                    text.append(elem.text)
-                    print(text)
-                elem.clear()
+                        if elem.text in ['2d Session']:
+                            hconres_session.append(
+                                elem.text.replace('2d Session', '2nd'))
+                        elif elem.text in ['At the Second Session']:
+                            hconres_session.append(elem.text.replace(
+                                'At the Second Session', '2nd'))
+                        else:
+                            print('You missed a session!')
+                elif elem.tag == 'resolution-body' and event == 'end':
+                    pass  # stuck here
+                # AMENDMENTS
+                elif elem.tag == 'amendment-doc' and event == 'end':
+                    amend_number.append(elem.get('legis-num'))
+                    amend_degree.append(elem.get('amend-degree'))
+                    amend_type.append(elem.get('legis-type'))
+                    amend_stage.append(elem.get('amend-type'))
+                elif elem.tag == 'legis-num' and event == 'end':
+                    amend_number.append(elem.text)
+                elif elem.tag == 'congress' and event == 'end':
+                    raw_congress = []
+                    raw_congress.append(elem.text)
+                    for r in raw_congress:
+                        if elem.text in ['115th CONGRESS']:
+                            amend_congress.append(elem.text.replace(
+                                '115th CONGRESS', '115th'))
+                        elif elem.text in ['One Hundred Fifteenth Congress of the United States of America']:
+                            amend_congress.append(elem.text.replace(
+                                'One Hundred Fifteenth Congress of the United States of America', '115th'))
+                        else:
+                            print('You missed a congress!')
+                elif elem.tag == 'session' and event == 'end':
+                    raw_session = []
+                    raw_session.append(elem.text)
+                    for r in raw_session:
+                        if elem.text in ['2d Session']:
+                            amend_session.append(
+                                elem.text.replace('2d Session', '2nd'))
+                        elif elem.text in ['At the Second Session']:
+                            amend_session.append(elem.text.replace(
+                                'At the Second Session', '2nd'))
+                        else:
+                            print('You missed a session!')
+                elif elem.tag == 'dc:publisher' and event == 'end':
+                    raw_publisher = []
+                    raw_publisher.append(elem.text)
+                    for r in raw_publisher:
+                        if elem.text in ['U.S. House of Representatives']:
+                            amend_origin.append(elem.text.replace(
+                                'U.S. House of Representatives', 'House'))
+                        else:
+                            print('You missed an origin!')
+                # elif elem.tag == 'text' and event == 'end':
+                    pass  # stuck here
+            elem.clear()
+            # print(hconres_dms_id)
+            # print(hconres_number)
+            # print(hconres_title)
+            # print(hconres_congress)
+            # print(hconres_session)
+            # print(hconres_stage)
+            # print(hconres_type)
+            # print(hconres_origin)
+            # print(hconres_pub_pri)
+            # print(hconres_text)
     remove_files(xml_files)
-    push_to_db(legis_num)
-    # except ET.ParseError as e:
-    # pass
+    push_to_db(hconres_dms_id, hconres_number, hconres_title, hconres_congress,
+               hconres_session, hconres_stage, hconres_type, hconres_origin, hconres_pub_pri, hconres_text)
 
 
 def remove_files(file):
@@ -77,7 +154,7 @@ def remove_files(file):
         os.remove(x)
 
 
-def push_to_db(legis_num, congress, session, text):
+def push_to_db(dms_id, legis_num, official_title, congress, session, resolution_stage, resolution_type, origin, public_private, hconres_text):
     success = bool
     if success:
         return 1
